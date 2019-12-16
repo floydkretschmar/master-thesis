766445f46bff91656091a3b916c41c49eec15d1b
Floyd Kretschmar <fkretschmar@pl-02.is.localnet>
2019-12-16 16:58:46 +0100

    added bulk data collection

--- src/consequential_learning.py
+++ src/consequential_learning.py
@@ -1,6 +1,6 @@
 import numpy as np
 from policy import LogisticPolicy
-from distribution import GroundTruthDistribution
+from distribution import SplitDistribution
 from util import get_minibatch
 
 DIM_X = 1
@@ -12,6 +12,7 @@ BATCH_SIZE = 128
 LEARNING_RATE = 0.01
 FAIRNESS_RATE = 0.5
 COST_FACTOR = 0.6
+FRACTION_PROTECTED = 0.3
 
 def consequential_learning(fairness_function):    
     """This is the main training loop for consequential learning.
@@ -19,24 +20,22 @@ def consequential_learning(fairness_function):
     Keyword arguments:
     fairness_function -- The function defining the fairness penalty.
     """
-    gt_dist = GroundTruthDistribution()
+    gt_dist = SplitDistribution()
     pi = LogisticPolicy(DIM_S + DIM_X, COST_FACTOR, fairness_function)
     for _ in range(0, T):        
-        data = collect_data(pi, gt_dist, NUM_DECISIONS)
+        data = collect_data(pi, gt_dist, NUM_DECISIONS, FRACTION_PROTECTED)
         pi.update(data, LEARNING_RATE, FAIRNESS_RATE, BATCH_SIZE, NUM_ITERATIONS)
 
-def collect_data(pi, gt_dist, N):
-    X = []
-    S = []
-    Y = []
-    for _ in range(0, N):
-        x, s = gt_dist.sample()
-        features = np.concatenate((x, s), axis=1)
-        d = pi(features)
-        if d == 1:
-            y = gt_dist(x, s)
-            X.append(x)
-            S.append(s)
-            Y.append(y)
+def collect_data(pi, gt_dist, num_samples, fraction_protected):
+    x, s = gt_dist.sample_features(num_samples, fraction_protected)
+    decisions = pi(x, s)
 
-    return np.array(X), np.array(S), np.array(Y)
+    pos_decision_idx = np.arange(x.shape[0])
+    pos_decision_idx = pos_decision_idx[decisions == 1]
+
+    x = x[pos_decision_idx]
+    s = s[pos_decision_idx]
+
+    y = gt_dist.sample_labels(x, s)
+
+    return x, s, y
