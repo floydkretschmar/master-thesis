970211b38474fa568e109e97d57216cc4ca5a2fb
Floyd Kretschmar <fkretschmar@pl-02.is.localnet>
2019-12-18 15:28:31 +0100

    added selective use of s for utility

--- src/consequential_learning.py
+++ src/consequential_learning.py
@@ -9,9 +9,9 @@ import numpy as np
 from src.policy import LogisticPolicy
 from src.distribution import SplitDistribution
 
-def collect_data(pi, gt_dist, num_samples, fraction_protected):
+def collect_data(pi, gt_dist, num_samples, fraction_protected, use_s):
     x, s = gt_dist.sample_features(num_samples, fraction_protected)
-    decisions = pi(x, s)
+    decisions = pi.make_decisions(x, s, use_s)
 
     pos_decision_idx = np.arange(x.shape[0])
     pos_decision_idx = pos_decision_idx[decisions == 1]
@@ -26,7 +26,7 @@ def collect_data(pi, gt_dist, num_samples, fraction_protected):
 def train(**training_args):
     gt_dist = SplitDistribution()
     learning_parameters = training_args["learning_parameters"]
-    pi = LogisticPolicy(training_args["dim_theta"], training_args["fairness_rate"], training_args["cost_factor"], training_args["fairness_function"], training_args["feature_map"])
+    pi = LogisticPolicy(training_args["fairness_rate"], training_args["cost_factor"], training_args["fairness_function"], training_args["feature_map"], training_args["dim_x"], training_args["dim_s"])
 
     learning_rate = learning_parameters["learning_rate"]
 
@@ -34,7 +34,7 @@ def train(**training_args):
         if i % learning_parameters['decay_step'] == 0:
             learning_rate *= learning_parameters['decay_rate']
 
-        data = collect_data(pi, gt_dist, training_args["num_decisions"], training_args["fraction_protected"])
+        data = collect_data(pi, gt_dist, training_args["num_decisions"], training_args["fraction_protected"], (training_args["dim_s"] is not None))
         utility, benefit_delta = pi.update(data, learning_rate, training_args["batch_size"])
 
         yield utility, benefit_delta
--- src/policy.py
+++ src/policy.py
@@ -12,22 +12,28 @@ from src.util import iterate_minibatches
 
 
 class BasePolicy():
-    def __init__(self, dim_theta, fairness_function): 
+    def __init__(self, fairness_function, dim_x, dim_s=None): 
         self.fairness_function = fairness_function
+        self.use_s = False
+
+        dim_theta = dim_x
+        if dim_s is not None:
+            self.use_s = True
+            dim_theta += dim_s
+
         self.theta = np.zeros(dim_theta)
     
     def benefit_function(self, x_s, s, sample_theta, gradient):
         raise NotImplementedError("Subclass must override calculate benefit_function(self, x_s, s, sample_theta, gradient).")
 
-    def __call__(self, x, s):
-        features = np.concatenate((x, s), axis=1)
+    def __call__(self, features):
         probability = self.calculate_probability(features)
         return np.random.binomial(1, probability)
 
     def calculate_benefit_delta(self, x, s, sample_theta):
-        return np.absolute(self.calculate_benefit_difference(x, s, sample_theta, False)).mean()
+        return np.absolute(self.calculate_benefit_difference(x, s, sample_theta, False, self.use_s)).mean()
 
-    def calculate_benefit_difference(self, x, s, sample_theta, gradient):
+    def calculate_benefit_difference(self, x, s, sample_theta, gradient, use_s):
         raise NotImplementedError("Subclass must override calculate benefit_difference(self, s, x, sample_theta, gradient).")
 
     def calculate_probability(self, features):
@@ -42,14 +48,15 @@ class BasePolicy():
     def calculate_utility(self, x, s, y, sample_theta):
         raise NotImplementedError("Subclass must override loss(self, x, s, y, sample_theta).")
 
-    def make_decisions(self, X_batch, S_batch):
-        decisions = self(X_batch, S_batch)
+    def make_decisions(self, X_batch, S_batch, use_s):
+        if self.use_s:
+            features = np.concatenate((X_batch, S_batch), axis=1)
+        else:
+            features = X_batch
 
-        # only use data where positive decisions have been made for gradient calculation
-        pos_decision_idx = np.arange(X_batch.shape[0])
-        pos_decision_idx = pos_decision_idx[decisions == 1]
+        decisions = self(features)
 
-        return pos_decision_idx
+        return decisions
 
     def update(self, data, learning_rate, batch_size):
         x, s, y = data
@@ -57,7 +64,11 @@ class BasePolicy():
 
         for X_batch, S_batch, Y_batch in iterate_minibatches(x, s, y, batch_size):
             # make decision according to current policy
-            pos_decision_idx = self.make_decisions(X_batch, S_batch)
+            decisions = self.make_decisions(X_batch, S_batch, self.use_s)
+
+            # only use data where positive decisions have been made for gradient calculation
+            pos_decision_idx = np.arange(X_batch.shape[0])
+            pos_decision_idx = pos_decision_idx[decisions == 1]
 
             # calculate the gradient
             gradient = self.calculate_gradient(X_batch[pos_decision_idx], S_batch[pos_decision_idx], Y_batch[pos_decision_idx], sample_theta)
@@ -72,33 +83,35 @@ class BasePolicy():
 
 
 class LogisticPolicy(BasePolicy):
-    def __init__(self, dim_theta, fairness_rate, cost_factor, fairness_function, feature_map): 
+    def __init__(self, fairness_rate, cost_factor, fairness_function, feature_map, dim_x, dim_s=None): 
         super(LogisticPolicy, self).__init__(
-            dim_theta,
-            fairness_function)
+            fairness_function,
+            dim_x,
+            dim_s)
 
         self.feature_map = feature_map
         self.fairness_rate = fairness_rate
         self.cost_factor = cost_factor
 
-    def benefit_function(self, x_s, s, sample_theta, gradient):
+    def benefit_function(self, x_s, s, sample_theta, gradient, use_s):
         ips_weight, phi, log_gradient_denominator = self.calculate_ips_weights_and_log_gradient(x_s, s, sample_theta)
-        decision = self(x_s, s).reshape(-1, 1)
+
+        decisions = self.make_decisions(x_s, s, use_s).reshape(-1, 1)
 
         if gradient:
-            grad_benefit = ((ips_weight/log_gradient_denominator) * decision * phi).sum(axis=0) / x_s.shape[0]
+            grad_benefit = ((ips_weight/log_gradient_denominator) * decisions * phi).sum(axis=0) / x_s.shape[0]
             return grad_benefit
         else:
-            benefit = (ips_weight * decision).sum(axis=0) / x_s.shape[0]
+            benefit = (ips_weight * decisions).sum(axis=0) / x_s.shape[0]
             return benefit
 
-    def calculate_benefit_difference(self, x, s, sample_theta, gradient):
+    def calculate_benefit_difference(self, x, s, sample_theta, gradient, use_s):
         pos_decision_idx = np.arange(s.shape[0]).reshape(-1, 1)
 
         s_0_idx = pos_decision_idx[s == 0]
         s_1_idx = pos_decision_idx[s == 1]
 
-        return self.benefit_function(x[s_0_idx], s[s_0_idx], sample_theta, gradient) - self.benefit_function(x[s_1_idx], s[s_1_idx], sample_theta, gradient)
+        return self.benefit_function(x[s_0_idx], s[s_0_idx], sample_theta, gradient, use_s) - self.benefit_function(x[s_1_idx], s[s_1_idx], sample_theta, gradient, use_s)
     
     def calculate_probability(self, features):
         return sigmoid(self.feature_map(features) @ self.theta)
@@ -111,7 +124,8 @@ class LogisticPolicy(BasePolicy):
             "x": x, 
             "s": s, 
             "sample_theta": sample_theta,
-            "policy": self
+            "policy": self,
+            "use_s": self.use_s
         }
 
         # get the gradient value of the fairness function
@@ -137,7 +151,11 @@ class LogisticPolicy(BasePolicy):
             numerator: numerator of the gradient of log pi which is the same as phi
             denominator: the denominator of the gradient of log pi defined as phi_i/(1+exp(phi_i @ theta_t))
         """
-        features = np.concatenate((x, s), axis=1)
+        if self.use_s:
+            features = np.concatenate((x, s), axis=1)
+        else:
+            features = x
+
         phi = self.feature_map(features)
         ones = np.ones(x.shape)
         sample_theta = sample_theta.reshape(-1, 1)
@@ -150,7 +168,8 @@ class LogisticPolicy(BasePolicy):
             "s": s, 
             "sample_theta": sample_theta,
             "policy": self,
-            "gradient": False
+            "gradient": False,
+            "use_s": self.use_s
         }
         fairness_pen = (self.fairness_rate * self.fairness_function(**fairness_params)**2)/2
         #print("Fairness penalty: {}".format(fairness_pen))
@@ -159,7 +178,9 @@ class LogisticPolicy(BasePolicy):
 
     def utility_function(self, x, s, y, sample_theta, gradient):
         ips_weight, phi, log_gradient_denominator = self.calculate_ips_weights_and_log_gradient(x, s, sample_theta)
-        utility_value = self(x, s).reshape(-1, 1) * (y - self.cost_factor)
+
+        decisions = self.make_decisions(x, s, self.use_s).reshape(-1, 1)
+        utility_value = decisions * (y - self.cost_factor)
 
         if gradient:
             # calculate the gradient of the utility function
