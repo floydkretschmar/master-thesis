c272233b40bb1c00974084650243b63bcf76377c
Floyd Kretschmar <fkretschmar@googlemail.com>
2020-01-17 14:55:14 +0100

    reworked logging of statistics

--- src/training.py
+++ src/training.py
@@ -7,11 +7,74 @@ if module_path not in sys.path:
 import numpy as np
 import multiprocessing as mp
 import copy
+from pathlib import Path
 
 from src.consequential_learning import consequential_learning
 from src.util import save_dictionary, load_dictionary
 
 result_list = []
+
+class TrainingStatistics():
+    def __init__(self):
+        self.mean_utilities = []
+        self.stddev_utilites = []
+        self.first_quartile_utilities = []
+        self.median_utilites = []
+        self.third_quartile_utilities = []
+        self.mean_benefit_delta = []
+        self.stddev_benefit_delta = []
+        self.first_quartile_benefit_delta = []
+        self.median_benefit_delta = []
+        self.third_quartile_benefit_delta = []
+        self.fairness_rates = []
+
+    def log_statistics(self, fairness_rate, utilities, benefit_deltas, verbose=False):
+        self.mean_utilities.append(utilities.mean())
+        self.stddev_utilites.append(utilities.std())
+        self.first_quartile_utilities.append(np.percentile(utilities, 25))
+        self.median_utilites.append(np.median(utilities))
+        self.third_quartile_utilities.append(np.percentile(utilities, 75))
+        self.mean_benefit_delta.append(benefit_deltas.mean())
+        self.stddev_benefit_delta.append(benefit_deltas.std())
+        self.first_quartile_benefit_delta.append(np.percentile(benefit_deltas, 25))
+        self.median_benefit_delta.append(np.median(benefit_deltas))
+        self.third_quartile_benefit_delta.append(np.percentile(benefit_deltas, 75))
+        self.fairness_rates.append(fairness_rate)
+
+        if verbose:
+            print("------------------- Utility ----------------------")
+            print("Mean: {}".format(utilities.mean()))
+            print("Standard deviation: {}".format(utilities.std()))
+            print("First quartile: {}".format(np.percentile(utilities, 25)))
+            print("Median: {}".format(np.median(utilities)))
+            print("Last quartile: {}".format(np.percentile(utilities, 75)))
+
+            print("------------------- Benefit Delta ----------------")
+            print("Mean: {}".format(benefit_deltas.mean()))
+            print("Standard deviation: {}".format(benefit_deltas.std()))
+            print("First quartile: {}".format(np.percentile(benefit_deltas, 25)))
+            print("Median: {}".format(np.median(benefit_deltas)))
+            print("Last quartile: {}".format(np.percentile(benefit_deltas, 75)))
+    
+    def to_json(self):
+        return {
+            "lambdas": self.fairness_rates,
+            "utility": {
+                "mean": np.array(self.mean_utilities),
+                "stddev": np.array(self.stddev_utilites),
+                "first_quartile": np.array(self.first_quartile_utilities),
+                "median": np.array(self.median_utilites),
+                "third_quartile": np.array(self.third_quartile_utilities),
+            },
+            "benefit_delta": {
+                "mean": np.array(self.mean_benefit_delta),
+                "stddev": np.array(self.stddev_benefit_delta),
+                "first_quartile": np.array(self.first_quartile_benefit_delta),
+                "median": np.array(self.median_benefit_delta),
+                "third_quartile": np.array(self.third_quartile_benefit_delta),
+            }
+        }
+
 
 def generate_data_set(training_parameters):
     num_decisions = training_parameters["data"]["num_decisions"]
@@ -39,23 +102,34 @@ def result_worker(result):
     global result_list
     result_list.append(result)
 
-def train_single(training_parameters):
+def _train_single(training_parameters, model_save_path=None):
     np.random.seed()
     for u, bd, pi in consequential_learning(**training_parameters):
         utility, benefit_delta, policy = u, bd, pi
 
     if "save_path" in training_parameters["model"]:
-        save_dictionary({"theta": policy.theta.tolist()}, training_parameters["model"]["save_path"])
+        save_dictionary({"theta": policy.theta.tolist()}, model_save_path)
 
     return utility, benefit_delta
 
 def train_multiple(training_parameters, iterations, verbose=False, asynchronous=True):
-    mean_utilities = []
-    stddev_utilites = []
-    mean_benefit_delta = []
-    stddev_benefit_delta = []
+    statistics = TrainingStatistics()
     current_train_parameters = copy.deepcopy(training_parameters)
-    base_model_save_path = training_parameters["model"]["save_path"]
+
+    if "save_path" in training_parameters["model"]:
+        base_path = training_parameters["model"]["save_path"]
+        runs = os.listdir(base_path)
+
+        if len(runs) == 0:
+            current_run = 0
+        else:
+            runs.sort()
+            current_run = int(runs[-1].replace("run", "")) + 1
+
+        model_save_directory = "{}/run{}/".format(base_path, current_run)
+        Path(model_save_directory).mkdir(parents=True, exist_ok=True)
+    else:
+        model_save_directory = None
 
     if current_train_parameters["data"]["keep_data_across_lambdas"]:
         current_train_parameters["data"] = generate_data_set(training_parameters)
@@ -63,29 +137,33 @@ def train_multiple(training_parameters, iterations, verbose=False, asynchronous=
     for fairness_rate in current_train_parameters["optimization"]["fairness_rates"]:
         global result_list
 
-        print("Processing Lambda: {}".format(fairness_rate))
+        if model_save_directory is not None:
+            model_save_directory = "{}/lambda{}/".format(model_save_directory, fairness_rate)
+            Path(model_save_directory).mkdir(parents=True, exist_ok=True)
+
+        print("--------------------------------------------------")
+        print("------------------- Lambda {} -------------------".format(fairness_rate))
+        print("--------------------------------------------------")
         benefit_deltas = []
         utilities = []
         current_train_parameters["optimization"]["fairness_rate"] = fairness_rate
-        
+
         # multithreaded runs of training
         if asynchronous:
             pool = mp.Pool(mp.cpu_count())
             for j in range(0, iterations):
-                model_save_path = "{}/lambda{}_model{}.json".format(base_model_save_path, fairness_rate, j)
-                if model_save_path is not None:
-                    current_train_parameters["model"]["save_path"] = model_save_path
+                if model_save_directory is not None:
+                    model_save_path = "{}/model_{}.json".format(model_save_directory, j)
 
-                pool.apply_async(train_single, args=(current_train_parameters,), callback=result_worker) 
+                pool.apply_async(_train_single, args=(current_train_parameters, model_save_path), callback=result_worker) 
             pool.close()
             pool.join()
         else:
             for j in range(0, iterations):
-                model_save_path = "{}/lambda{}_model{}.json".format(base_model_save_path, fairness_rate, j)
-                if model_save_path is not None:
-                    current_train_parameters["model"]["save_path"] = model_save_path
+                if model_save_directory is not None:
+                    model_save_path = "{}/model_{}.json".format(model_save_directory, j)
 
-                result = train_single(current_train_parameters)
+                result = _train_single(current_train_parameters, model_save_path)
                 result_worker(result)
 
         results = np.array(result_list).squeeze()
@@ -94,24 +172,6 @@ def train_multiple(training_parameters, iterations, verbose=False, asynchronous=
         utilities = results[:,0]
         benefit_deltas = results[:,1]
 
-        mean_utilities.append(utilities.mean())
-        stddev_utilites.append(utilities.std())
-        mean_benefit_delta.append(benefit_deltas.mean())
-        stddev_benefit_delta.append(benefit_deltas.std())
+        statistics.log_statistics(fairness_rate, utilities, benefit_deltas, verbose)
 
-        if verbose:
-            print("Mean utility: {}".format(utilities.mean()))
-            print("Stddev utility: {}".format(utilities.std()))
-            print("Mean benefit delta: {}".format(benefit_deltas.mean()))
-            print("Stddev benefit delta: {}".format(benefit_deltas.std()))
-
-    return {
-        "utility_stats": {
-            "mean": np.array(mean_utilities),
-            "stddev": np.array(stddev_utilites)
-        },
-        "benefit_delta_stats": {
-            "mean": np.array(mean_benefit_delta),
-            "stddev": np.array(stddev_benefit_delta)
-        }
-    }
+    return statistics.to_json()
