e65524908cba4a32ffdcc18643c63a572e1cce57
Floyd Kretschmar <fkretschmar@googlemail.com>
2020-01-17 14:55:25 +0100

    reworked plotting

--- src/plotting.py
+++ src/plotting.py
@@ -14,21 +14,40 @@ def plot_results_over_time(utility, benefit_delta):
     ax2.set_ylabel("Mean Benefit Delta")
     plt.show()
 
-#def plot_results_over_lambdas(utility_mean, utility_max, utility_min, benefit_delta_mean, benefit_delta_max, benefit_delta_min, lambdas, file_path=None):
-def plot_results_over_lambdas(utility_mean, utility_stddev, benefit_delta_mean, benefit_stddev, lambdas, file_path=None):
+def _plot_results_over_lambdas(utility, benefit_delta, lambdas, utility_uncertainty=None, benefit_delta_uncertainty=None, file_path=None):
     f, (ax1, ax2) = plt.subplots(1, 2, sharex=True, figsize=(25, 10))
-    ax1.plot(lambdas, utility_mean)
+    ax1.plot(lambdas, utility)
     ax1.set_xlabel("Lambda")
     ax1.set_ylabel("Utility")
     ax1.set_xscale('log')
-    ax1.fill_between(lambdas, utility_mean-utility_stddev, utility_mean+utility_stddev, alpha=0.3, edgecolor='#060080', facecolor='#928CFF')
-    ax2.plot(lambdas, benefit_delta_mean)
+    ax1.fill_between(lambdas, utility_uncertainty[0], utility_uncertainty[1], alpha=0.3, edgecolor='#060080', facecolor='#928CFF')
+    ax2.plot(lambdas, benefit_delta)
     ax2.set_xlabel("Lambda")
     ax2.set_ylabel("Mean Benefit Delta")
     ax2.set_xscale('log')
-    ax2.fill_between(lambdas, benefit_delta_mean-benefit_stddev, benefit_delta_mean+benefit_stddev, alpha=0.3, edgecolor='#060080', facecolor='#928CFF')
+    ax2.fill_between(lambdas, benefit_delta_uncertainty[0], benefit_delta_uncertainty[1], alpha=0.3, edgecolor='#060080', facecolor='#928CFF')
 
     if file_path is None:
         plt.show()
     else:
         plt.savefig(file_path)
+
+def plot_mean_over_lambdas(statistics, file_path=None):
+    util_mean = statistics["utility"]["mean"]
+    ben_mean = statistics["benefit_delta"]["mean"]
+    _plot_results_over_lambdas(
+        util_mean,
+        ben_mean,
+        statistics["lambdas"],
+        (util_mean - statistics["utility"]["stddev"], util_mean + statistics["utility"]["stddev"]),
+        (ben_mean - statistics["benefit_delta"]["stddev"], ben_mean + statistics["benefit_delta"]["stddev"]),
+        file_path)
+
+def plot_median_over_lambdas(statistics, file_path=None):
+    _plot_results_over_lambdas(
+        statistics["utility"]["median"],
+        statistics["benefit_delta"]["median"],
+        statistics["lambdas"],
+        (statistics["utility"]["first_quartile"], statistics["utility"]["third_quartile"]),
+        (statistics["benefit_delta"]["first_quartile"], statistics["benefit_delta"]["third_quartile"]),
+        file_path)
