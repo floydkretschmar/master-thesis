1416804fc0e2f2cff7c15449197f4e2bff2de0a8
Floyd Kretschmar <fkretschmar@googlemail.com>
2020-01-17 10:33:45 +0100

    added save and load methods

--- src/util.py
+++ src/util.py
@@ -1,7 +1,23 @@
 import numpy as np
+import json
 np.seterr(divide='ignore', invalid='ignore', over='ignore')
 
 def sigmoid(x):
     return np.where(x >= 0, 
                     1 / (1 + np.exp(-x)), 
                     np.exp(x) / (1 + np.exp(x)))
+
+def save_dictionary(dictionary, path):
+    try:
+        with open(path, 'w') as file_path:
+            json.dump(dictionary, file_path)
+    except Exception as e:
+        print('Saving file {} failed with exception: \n {}'.format(path, str(e)))
+
+def load_dictionary(path):
+    try:
+        with open(path, 'r') as file_path:
+            return json.load(file_path)
+    except Exception as e:
+        print('Loading file {} failed with exception: \n {}'.format(path, str(e)))
+        return None
