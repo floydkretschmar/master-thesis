cbbe8f9e51c7b51bceac22865e5a233527bc8237
Floyd Kretschmar <fkretschmar@googlemail.com>
2020-01-23 15:54:58 +0100

    changed test

--- src/test.py
+++ src/test.py
@@ -8,10 +8,34 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from src.feature_map import IdentityFeatureMap
 from src.functions import cost_utility, demographic_parity
-from src.plotting import plot_median_over_time
+from src.plotting import plot_median_over_time, plot_median_over_lambdas
 from src.training import train
 import multiprocessing as mp
 from src.distribution import SplitDistribution, UncalibratedScore
+
+def fairness_function(**fairness_kwargs):
+    policy = fairness_kwargs["policy"]
+    x = fairness_kwargs["x"]
+    s = fairness_kwargs["s"]
+    y = fairness_kwargs["y"]
+    decisions = fairness_kwargs["decisions"]
+    ips_weights = fairness_kwargs["ips_weights"]
+    numerator, denominator_log_grad = policy._log_gradient(x, s)
+
+    benefit = policy.benefit_function(decisions=decisions, y=y)
+
+    if ips_weights is not None:
+        mu_s = np.mean(s * ips_weights, axis=0)
+    else:
+        mu_s = np.mean(s, axis=0)
+
+    if ips_weights is not None:
+        benefit *= ips_weights
+
+    covariance = (s - mu_s) * benefit
+    covariance_grad = (covariance * numerator)/denominator_log_grad
+
+    return np.mean(covariance, axis=0) * np.mean(covariance_grad, axis=0)
 
 # def fairness_function(**fairness_kwargs):
 #     policy = fairness_kwargs["policy"]
@@ -21,46 +45,17 @@ from src.distribution import SplitDistribution, UncalibratedScore
 #     decisions = fairness_kwargs["decisions"]
 #     ips_weights = fairness_kwargs["ips_weights"]
 
-#     phi = policy.feature_map(policy._extract_features(x, s))
-#     denominator = np.expand_dims((1.0 + np.exp(np.matmul(phi, policy.theta))), axis=1)
 #     benefit = policy.benefit_function(decisions=decisions, y=y)
-
-#     # if ips_weights is not None:
-#     #     mu_s = np.mean(s * ips_weights, axis=0)
-#     # else:
-#     #     mu_s = np.mean(s, axis=0)
-#     mu_s = np.mean(s, axis=0)
-
-#     covariance = (s - mu_s) * benefit
-#     covariance_grad = covariance * phi
+#     log_gradient = policy._log_gradient(x, s)
+    
+#     benefit_grad = benefit * log_gradient
 
 #     if ips_weights is not None:
-#         #covariance *= ips_weights
-#         covariance_grad *= ips_weights/denominator
-#     else:
-#         covariance_grad /= denominator
-
-#     return np.mean(covariance, axis=0) * np.mean(covariance_grad, axis=0)
-
-def fairness_function(**fairness_kwargs):
-    policy = fairness_kwargs["policy"]
-    x = fairness_kwargs["x"]
-    s = fairness_kwargs["s"]
-    y = fairness_kwargs["y"]
-    decisions = fairness_kwargs["decisions"]
-    ips_weights = fairness_kwargs["ips_weights"]
-
-    benefit = policy.benefit_function(decisions=decisions, y=y)
-    log_gradient = policy._log_gradient(x, s)
-    
-    benefit_grad = benefit * log_gradient
-
-    if ips_weights is not None:
-        benefit *= ips_weights
-        benefit_grad *= ips_weights
+#         benefit *= ips_weights
+#         benefit_grad *= ips_weights
         
-    # benefit-difference * grad-benefit-difference
-    return policy._mean_difference(benefit, s) * policy._mean_difference(benefit_grad, s)
+#     # benefit-difference * grad-benefit-difference
+#     return policy._mean_difference(benefit, s) * policy._mean_difference(benefit_grad, s)
 
 i = 1
 bias = True
@@ -68,11 +63,10 @@ dim_x = 1
 dim_theta = dim_x + 1 if bias else dim_x
 
 def util_func(**util_params):
-    util = cost_utility(cost_factor=0.1, **util_params)
+    util = cost_utility(cost_factor=0.142, **util_params)
     return util
 
 training_parameters = {    
-    'save_path': "/home/fkretschmar/Documents/master-thesis/res/exp-006/uncalibrated",
     'model':{
         'theta': [-3.0, 5.0],
         'benefit_value_function': demographic_parity,
@@ -84,13 +78,12 @@ training_parameters = {
         'bias': bias
     },
     'optimization': {
-        'time_steps':5,
-        'epochs': 128,
+        'time_steps':200,
+        'epochs': 1,
         'batch_size':256,
         'learning_rate': 1,
         'decay_rate': 1,
-        'decay_step': 10000,
-        'test_at_every_timestep': False
+        'decay_step': 10000
     },
     'data': {
         'distribution': UncalibratedScore(bias=bias),
@@ -101,7 +94,12 @@ training_parameters = {
     }
 }
 
+training_parameters["save_path"] = "/home/fkretschmar/Documents/master-thesis/res/exp-008/uncalibrated/lambda"
+lambdas = np.logspace(-1, 1, base=10, endpoint=True, num=100)
+lambdas = np.insert(arr=lambdas, obj=0, values=[0.0])
 
-statistics, run_path = train(training_parameters, fairness_rates=[0.0], iterations=2, store_all=True, verbose=True, asynchronous=False)
+statistics, run_path = train(training_parameters, fairness_rates=lambdas, iterations=5, verbose=True, asynchronous=True)
 
-plot_median_over_time(statistics, "{}/results_median.png".format(run_path))
+plot_median_over_lambdas(statistics, "{}/results_median_lambdas.png".format(run_path))
+
+#plot_median_over_time(statistics, "{}/results_median.png".format(run_path))
