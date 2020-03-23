found bug in implementation of covariance function

src/test.py
@@ -9,78 +9,59 @@ from src.functions import cost_utility, demographic_parity
 from src.plotting import plot_median
 from src.training import train
 from src.distribution import UncalibratedScore
+import numpy as np
 
 
-# def fairness_function(**fairness_kwargs):
-#     policy = fairness_kwargs["policy"]
-#     x = fairness_kwargs["x"]
-#     s = fairness_kwargs["s"]
-#     y = fairness_kwargs["y"]
-#     decisions = fairness_kwargs["decisions"]
-#     ips_weights = fairness_kwargs["ips_weights"]
-#     numerator, denominator_log_grad = policy._log_gradient(x, s)
-
-#     benefit = policy.benefit_function(decisions=decisions, y=y)
-
-#     if ips_weights is not None:
-#         mu_s = np.mean(s * ips_weights, axis=0)
-#     else:
-#         mu_s = np.mean(s, axis=0)
-
-#     if ips_weights is not None:
-#         benefit *= ips_weights
-
-#     covariance = (s - mu_s) * benefit
-#     covariance_grad = (covariance * numerator)/denominator_log_grad
-
-#     return np.mean(covariance, axis=0), np.mean(covariance_grad, axis=0)
-
-def calc_benefit(**fairness_kwargs):
-    y = fairness_kwargs["y"]
-    decisions = fairness_kwargs["decisions"]
-    ips_weights = fairness_kwargs["ips_weights"]
-
-    benefit = demographic_parity(decisions=decisions, y=y)
+def calc_covariance(x, s, policy, ips_weights):
+    phi = policy.feature_map(policy._extract_features(x, s))
+    distance = np.matmul(phi, policy.theta).reshape(-1, 1)
 
     if ips_weights is not None:
-        benefit *= ips_weights
+        mu_s = np.mean(s * ips_weights, axis=0)
+        distance *= ips_weights
+    else:
+        mu_s = np.mean(s, axis=0)
 
-    return benefit
+    covariance = (s - mu_s) * distance
+    return covariance
 
 
 def fairness_gradient_function(**fairness_kwargs):
     policy = fairness_kwargs["policy"]
     x = fairness_kwargs["x"]
     s = fairness_kwargs["s"]
-    benefit = calc_benefit(**fairness_kwargs)
+    ips_weights = fairness_kwargs["ips_weights"]
 
-    log_gradient = policy.log_policy_gradient(x, s)
-    benefit_grad = log_gradient * benefit
+    covariance = calc_covariance(x, s, policy, ips_weights)
 
-    return policy._mean_difference(benefit_grad, s)
+    log_policy_gradient = policy.log_policy_gradient(x, s)
+    covariance_grad = log_policy_gradient * covariance
+
+    return np.mean(covariance_grad, axis=0)
 
 
 def fairness_function(**fairness_kwargs):
     policy = fairness_kwargs["policy"]
+    x = fairness_kwargs["x"]
     s = fairness_kwargs["s"]
-    benefit = calc_benefit(**fairness_kwargs)
+    ips_weights = fairness_kwargs["ips_weights"]
 
-    return policy._mean_difference(benefit, s)
+    covariance = calc_covariance(x, s, policy, ips_weights)
+    return np.mean(covariance, axis=0)
 
 
 bias = True
 dim_x = 1
 dim_theta = dim_x + 1 if bias else dim_x
 
-
 def util_func(**util_params):
     util = cost_utility(cost_factor=0.142, **util_params)
     return util
 
-
 training_parameters = {
-    'save_path': './',
+    'save': True,
     'experiment_name': 'test',
+    'save_path': './',
     'model': {
         'benefit_function': demographic_parity,
         'utility_function': util_func,
@@ -93,7 +74,7 @@ training_parameters = {
         'initial_theta': [-3.0, 5.0]
     },
     'parameter_optimization': {
-        'time_steps': 200,
+        'time_steps': 50,
         'epochs': 1,
         'batch_size': 256,
         'learning_rate': 1,
@@ -106,9 +87,80 @@ training_parameters = {
         'num_test_samples': 8192
     }
 }
+# def calc_benefit(**fairness_kwargs):
+#     y = fairness_kwargs["y"]
+#     decisions = fairness_kwargs["decisions"]
+#     ips_weights = fairness_kwargs["ips_weights"]
+#
+#     benefit = demographic_parity(decisions=decisions, y=y)
+#
+#     if ips_weights is not None:
+#         benefit *= ips_weights
+#
+#     return benefit
+#
+#
+# def fairness_gradient_function(**fairness_kwargs):
+#     policy = fairness_kwargs["policy"]
+#     x = fairness_kwargs["x"]
+#     s = fairness_kwargs["s"]
+#     benefit = calc_benefit(**fairness_kwargs)
+#
+#     log_gradient = policy.log_policy_gradient(x, s)
+#     benefit_grad = log_gradient * benefit
+#
+#     return policy._mean_difference(benefit_grad, s)
+#
+#
+# def fairness_function(**fairness_kwargs):
+#     policy = fairness_kwargs["policy"]
+#     s = fairness_kwargs["s"]
+#     benefit = calc_benefit(**fairness_kwargs)
+#
+#     return policy._mean_difference(benefit, s)
 
-# lambdas = np.logspace(-2, 1, base=10, endpoint=True, num=20)
-# #lambdas = np.insert(arr=lambdas, obj=0, values=[0.0])
+
+# bias = True
+# dim_x = 1
+# dim_theta = dim_x + 1 if bias else dim_x
+#
+#
+# def util_func(**util_params):
+#     util = cost_utility(cost_factor=0.142, **util_params)
+#     return util
+#
+#
+# training_parameters = {
+#     'save_path': './',
+#     'experiment_name': 'test',
+#     'model': {
+#         'benefit_function': demographic_parity,
+#         'utility_function': util_func,
+#         'fairness_function': fairness_function,
+#         'fairness_gradient_function': fairness_gradient_function,
+#         'feature_map': IdentityFeatureMap(dim_theta),
+#         'learn_on_entire_history': False,
+#         'use_sensitve_attributes': False,
+#         'bias': bias,
+#         'initial_theta': [-3.0, 5.0]
+#     },
+#     'parameter_optimization': {
+#         'time_steps': 200,
+#         'epochs': 1,
+#         'batch_size': 256,
+#         'learning_rate': 1,
+#         'decay_rate': 1,
+#         'decay_step': 10000,
+#         'num_decisions': 128 * 256
+#     },
+#     'data': {
+#         'distribution': UncalibratedScore(bias=bias, fraction_protected=0.5),
+#         'num_test_samples': 8192
+#     }
+# }
+
+# lambdas = np.logspace(-4, -3, base=10, endpoint=True, num=20)
+# lambdas = np.insert(arr=lambdas, obj=0, values=[0.0])
 # training_parameters["model"]["initial_lambda"] = lambdas
 
 # training_parameters["experiment_name"] = "Test"
@@ -127,19 +179,16 @@ training_parameters = {
 # x, x_test, y, y_test, s, s_test = train_test_split(x, y, s, test_size=0.8)
 # dist = ResamplingDistribution(load_dataset("./dat/compas/compas.npz"), 0.2, bias=bias)
 
-training_parameters["model"]["initial_lambda"] = 0.0
+# training_parameters["model"]["initial_lambda"] = 0.000007799
+training_parameters["model"]["initial_lambda"] = 0.001
 
-# lambdas = np.logspace(-2, 1, base=10, endpoint=True, num=19)
+# training_parameters["save_path"] = "/home/fkretschmar/Documents/master-thesis/res/test/uncalibrated/time"
+# lambdas = np.logspace(-1, 1, base=10, endpoint=True, num=3)
 # lambdas = np.insert(arr=lambdas, obj=0, values=[0.0])
-# training_parameters["optimization"]["parameters"]["lambda"] = lambdas
-
-#training_parameters["save_path"] = "/home/fkretschmar/Documents/master-thesis/res/test/uncalibrated/time"
-#lambdas = np.logspace(-1, 1, base=10, endpoint=True, num=3)
-#lambdas = np.insert(arr=lambdas, obj=0, values=[0.0])
 
 statistics, model_parameters, run_path = train(training_parameters, iterations=10, asynchronous=False)
-#statistics, run_path = train(training_parameters, fairness_rates=lambdas, iterations=5, verbose=True, asynchronous=False)
-#statistics, run_path = train(training_parameters, fairness_rates=[0.0], iterations=5, verbose=True, asynchronous=False)
+# statistics, run_path = train(training_parameters, fairness_rates=lambdas, iterations=5, verbose=True, asynchronous=False)
+# statistics, run_path = train(training_parameters, fairness_rates=[0.0], iterations=5, verbose=True, asynchronous=False)
 
 #plot_median(statistics, model_parameters=model_parameters)
 plot_median(statistics, model_parameters=None)