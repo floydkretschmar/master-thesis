d00970dfdcdb44b0980951450e415f4c34a213e1
Floyd Kretschmar <fkretschmar@googlemail.com>
2020-04-27 12:35:30 +0200

    changed test

--- src/test.py
+++ src/test.py
@@ -4,50 +4,89 @@ import sys
 root_path = os.path.abspath(os.path.join('.'))
 if root_path not in sys.path:
     sys.path.append(root_path)
-
-from src.feature_map import IdentityFeatureMap
-from src.functions import cost_utility, demographic_parity
-from src.plotting import plot_median
-from src.training import train
-from src.distribution import UncalibratedScore
-import numpy as np
-
-
-# def calc_benefit(decisions, y, ips_weights):
-#     benefit = demographic_parity(decisions=decisions, y=y)
 #
-#     if ips_weights is not None:
-#         benefit *= ips_weights
-#
-#     return benefit
+# from src.feature_map import IdentityFeatureMap
+# from src.functions import cost_utility, demographic_parity
+# from src.plotting import plot_median
+# from src.training import train
+# from src.distribution import UncalibratedScore
+# import numpy as np
 #
 #
-# def fairness_gradient_function(**fairness_kwargs):
-#     policy = fairness_kwargs["policy"]
-#     x = fairness_kwargs["x"]
-#     s = fairness_kwargs["s"]
-#     y = fairness_kwargs["y"]
-#     decisions = fairness_kwargs["decisions"]
-#     ips_weights = fairness_kwargs["ips_weights"]
-#     benefit = calc_benefit(decisions, y, ips_weights)
+# # def calc_benefit(decisions, y, ips_weights):
+# #     benefit = demographic_parity(decisions=decisions, y=y)
+# #
+# #     if ips_weights is not None:
+# #         benefit *= ips_weights
+# #
+# #     return benefit
+# #
+# #
+# # def fairness_gradient_function(**fairness_kwargs):
+# #     policy = fairness_kwargs["policy"]
+# #     x = fairness_kwargs["x"]
+# #     s = fairness_kwargs["s"]
+# #     y = fairness_kwargs["y"]
+# #     decisions = fairness_kwargs["decisions"]
+# #     ips_weights = fairness_kwargs["ips_weights"]
+# #     benefit = calc_benefit(decisions, y, ips_weights)
+# #
+# #     log_gradient = policy.log_policy_gradient(x, s)
+# #     benefit_grad = log_gradient * benefit
+# #
+# #     return mean_difference(benefit_grad, s)
+# #
+# #
+# # def fairness_function(**fairness_kwargs):
+# #     s = fairness_kwargs["s"]
+# #     y = fairness_kwargs["y"]
+# #     decisions = fairness_kwargs["decisions"]
+# #     ips_weights = fairness_kwargs["ips_weights"]
+# #     benefit = calc_benefit(decisions, y, ips_weights)
+# #
+# #     return mean_difference(benefit, s)
 #
-#     log_gradient = policy.log_policy_gradient(x, s)
-#     benefit_grad = log_gradient * benefit
 #
-#     return mean_difference(benefit_grad, s)
+# # def calc_covariance(x, s, policy, ips_weights):
+# #     phi = policy.feature_map(policy._extract_features(x, s))
+# #     distance = np.matmul(phi, policy.theta).reshape(-1, 1)
+# #
+# #     if ips_weights is not None:
+# #         mu_s = np.mean(s * ips_weights, axis=0)
+# #         distance *= ips_weights
+# #     else:
+# #         mu_s = np.mean(s, axis=0)
+# #
+# #     covariance = (s - mu_s) * distance
+# #     return covariance, distance
+# #
+# #
+# # def fairness_gradient_function(**fairness_kwargs):
+# #     policy = fairness_kwargs["policy"]
+# #     x = fairness_kwargs["x"]
+# #     s = fairness_kwargs["s"]
+# #     ips_weights = fairness_kwargs["ips_weights"]
+# #
+# #     covariance, distance = calc_covariance(x, s, policy, ips_weights)
+# #
+# #     log_policy_gradient = policy.log_policy_gradient(x, s)
+# #     covariance_grad = log_policy_gradient * covariance
+# #
+# #     # return np.mean(covariance_grad, axis=0) / distance.std()
+# #     return np.mean(covariance_grad, axis=0) / distance.std() if distance.std() != 0 else np.mean(covariance_grad, axis=0)
+# #
+# #
+# # def fairness_function(**fairness_kwargs):
+# #     policy = fairness_kwargs["policy"]
+# #     x = fairness_kwargs["x"]
+# #     s = fairness_kwargs["s"]
+# #     ips_weights = fairness_kwargs["ips_weights"]
+# #
+# #     covariance, distance = calc_covariance(x, s, policy, ips_weights)
+# #     # return np.mean(covariance, axis=0) / distance.std()
+# #     return np.mean(covariance, axis=0) / distance.std() if distance.std() != 0 else np.mean(covariance, axis=0)
 #
-#
-# def fairness_function(**fairness_kwargs):
-#     s = fairness_kwargs["s"]
-#     y = fairness_kwargs["y"]
-#     decisions = fairness_kwargs["decisions"]
-#     ips_weights = fairness_kwargs["ips_weights"]
-#     benefit = calc_benefit(decisions, y, ips_weights)
-#
-#     return mean_difference(benefit, s)
-
-
-# def calc_covariance(x, s, policy, ips_weights):
+# def calc_covariance(x, s, policy, ips_weights, decisions):
 #     phi = policy.feature_map(policy._extract_features(x, s))
 #     distance = np.matmul(phi, policy.theta).reshape(-1, 1)
 #
@@ -57,8 +96,9 @@ import numpy as np
 #     else:
 #         mu_s = np.mean(s, axis=0)
 #
+#     # covariance = (s - mu_s) * decisions
 #     covariance = (s - mu_s) * distance
-#     return covariance, distance
+#     return covariance
 #
 #
 # def fairness_gradient_function(**fairness_kwargs):
@@ -66,14 +106,14 @@ import numpy as np
 #     x = fairness_kwargs["x"]
 #     s = fairness_kwargs["s"]
 #     ips_weights = fairness_kwargs["ips_weights"]
+#     decisions = fairness_kwargs["decisions"]
 #
-#     covariance, distance = calc_covariance(x, s, policy, ips_weights)
+#     covariance = calc_covariance(x, s, policy, ips_weights, decisions)
 #
 #     log_policy_gradient = policy.log_policy_gradient(x, s)
 #     covariance_grad = log_policy_gradient * covariance
 #
-#     # return np.mean(covariance_grad, axis=0) / distance.std()
-#     return np.mean(covariance_grad, axis=0) / distance.std() if distance.std() != 0 else np.mean(covariance_grad, axis=0)
+#     return np.mean(covariance_grad, axis=0)
 #
 #
 # def fairness_function(**fairness_kwargs):
@@ -81,65 +121,65 @@ import numpy as np
 #     x = fairness_kwargs["x"]
 #     s = fairness_kwargs["s"]
 #     ips_weights = fairness_kwargs["ips_weights"]
+#     decisions = fairness_kwargs["decisions"]
 #
-#     covariance, distance = calc_covariance(x, s, policy, ips_weights)
-#     # return np.mean(covariance, axis=0) / distance.std()
-#     return np.mean(covariance, axis=0) / distance.std() if distance.std() != 0 else np.mean(covariance, axis=0)
-
-def calc_covariance(x, s, policy, ips_weights, decisions):
-    phi = policy.feature_map(policy._extract_features(x, s))
-    distance = np.matmul(phi, policy.theta).reshape(-1, 1)
-
-    if ips_weights is not None:
-        mu_s = np.mean(s * ips_weights, axis=0)
-        distance *= ips_weights
-    else:
-        mu_s = np.mean(s, axis=0)
-
-    # covariance = (s - mu_s) * decisions
-    covariance = (s - mu_s) * distance
-    return covariance
-
-
-def fairness_gradient_function(**fairness_kwargs):
-    policy = fairness_kwargs["policy"]
-    x = fairness_kwargs["x"]
-    s = fairness_kwargs["s"]
-    ips_weights = fairness_kwargs["ips_weights"]
-    decisions = fairness_kwargs["decisions"]
-
-    covariance = calc_covariance(x, s, policy, ips_weights, decisions)
-
-    log_policy_gradient = policy.log_policy_gradient(x, s)
-    covariance_grad = log_policy_gradient * covariance
-
-    return np.mean(covariance_grad, axis=0)
-
-
-def fairness_function(**fairness_kwargs):
-    policy = fairness_kwargs["policy"]
-    x = fairness_kwargs["x"]
-    s = fairness_kwargs["s"]
-    ips_weights = fairness_kwargs["ips_weights"]
-    decisions = fairness_kwargs["decisions"]
-
-    covariance = calc_covariance(x, s, policy, ips_weights, decisions)
-    return np.mean(covariance, axis=0)
-
-
+#     covariance = calc_covariance(x, s, policy, ips_weights, decisions)
+#     return np.mean(covariance, axis=0)
+#
+#
+# # bias = True
+# # distribution = ResamplingDistribution(bias=bias, dataset=load_dataset("../dat/compas/compas.npz"), test_percentage=0.2)
+# # dim_theta = distribution.feature_dim
+#
+# #
+# # def util_func(**util_params):
+# #     util = cost_utility(cost_factor=0.6, **util_params)
+# #     return util
+#
+#
+# # training_parameters = {
+# #     'save_path': './',
+# #     'experiment_name': 'test',
+# #     'model': {
+# #         'benefit_function': demographic_parity,
+# #         'utility_function': util_func,
+# #         'fairness_function': fairness_function,
+# #         'fairness_gradient_function': fairness_gradient_function,
+# #         'feature_map': IdentityFeatureMap(dim_theta),
+# #         'learn_on_entire_history': False,
+# #         'use_sensitve_attributes': False,
+# #         'bias': bias,
+# #         'initial_theta': np.zeros((dim_theta))
+# #     },
+# #     'parameter_optimization': {
+# #         'time_steps': 200,
+# #         'epochs': 40,
+# #         'batch_size': 256,
+# #         'learning_rate': 0.01,
+# #         'decay_rate': 1,
+# #         'decay_step': 10000,
+# #         'num_decisions': 4096
+# #     },
+# #     'data': {
+# #         'distribution': distribution,
+# #         'num_test_samples': None
+# #     }
+# # }
+#
 # bias = True
-# distribution = ResamplingDistribution(bias=bias, dataset=load_dataset("../dat/compas/compas.npz"), test_percentage=0.2)
-# dim_theta = distribution.feature_dim
-
+# dim_x = 1
+# dim_theta = dim_x + 1 if bias else dim_x
+#
 #
 # def util_func(**util_params):
-#     util = cost_utility(cost_factor=0.6, **util_params)
+#     util = cost_utility(cost_factor=0.142, **util_params)
 #     return util
-
-
+#
+#
 # training_parameters = {
-#     'save_path': './',
+#     'save': True,
 #     'experiment_name': 'test',
+#     'save_path': './',
 #     'model': {
 #         'benefit_function': demographic_parity,
 #         'utility_function': util_func,
@@ -149,93 +189,133 @@ def fairness_function(**fairness_kwargs):
 #         'learn_on_entire_history': False,
 #         'use_sensitve_attributes': False,
 #         'bias': bias,
-#         'initial_theta': np.zeros((dim_theta))
+#         'initial_theta': [0.0, 0.0]
 #     },
 #     'parameter_optimization': {
-#         'time_steps': 200,
-#         'epochs': 40,
+#         'time_steps': 50,
+#         'epochs': 1,
 #         'batch_size': 256,
-#         'learning_rate': 0.01,
+#         'learning_rate': 1,
 #         'decay_rate': 1,
 #         'decay_step': 10000,
-#         'num_decisions': 4096
+#         'num_decisions': 128 * 256
 #     },
 #     'data': {
-#         'distribution': distribution,
-#         'num_test_samples': None
+#         'distribution': UncalibratedScore(bias=bias, fraction_protected=0.5),
+#         'num_test_samples': 8192
 #     }
 # }
+#
+#
+# # x, s, y = load_dataset("./src/dat/compas/compas.npz")
+# # x, x_test, y, y_test, s, s_test = train_test_split(x, y, s, test_size=0.8)
+# # dist = ResamplingDistribution(load_dataset("./dat/compas/compas.npz"), 0.2, bias=bias)
+#
+# # training_parameters["model"]["initial_lambda"] = 0.000007799
+# training_parameters["model"]["initial_lambda"] = 0
+#
+# # training_parameters["save_path"] = "/home/fkretschmar/Documents/master-thesis/res/test/uncalibrated/time"
+# # lambdas = np.logspace(-1, 1, base=10, endpoint=True, num=3)
+# # lambdas = np.insert(arr=lambdas, obj=0, values=[0.0])
+# training_parameters["lagrangian_optimization"] = {
+#     # 'duality_gap': 0.00001,
+#     'iterations': 20,
+#     'epochs': 1,
+#     'batch_size': 256,
+#     'learning_rate': 0.01,
+#     'decay_rate': 1,
+#     'decay_step': 10000,
+#     'num_decisions': 128 * 256
+# }
+#
+# statistics, model_parameters, run_path = train(training_parameters, iterations=3, asynchronous=False)
+# # statistics, run_path = train(training_parameters, fairness_rates=lambdas, iterations=5, verbose=True, asynchronous=False)
+# # statistics, run_path = train(training_parameters, fairness_rates=[0.0], iterations=5, verbose=True, asynchronous=False)
+#
+# #plot_median(statistics, model_parameters=model_parameters)
+# plot_median(statistics, model_parameters=None)
+# #plot_mean(statistics, "{}/results_mean_lambdas.png".format(run_path))
+#
+# #plot_median_over_lambdas(statistics, "{}/results_median_lambdas.png".format(run_path))
+#
+# #plot_median_over_time(statistics, "{}/results_median.png".format(run_path))
 
-bias = True
-dim_x = 1
-dim_theta = dim_x + 1 if bias else dim_x
+# import numpy as np
+# import pandas as pd
+# from responsibly.dataset import build_FICO_dataset
+#
+# fico_data = build_FICO_dataset()
+# fico_cdf = fico_data["cdf"]
+# PRECISION = 4
+# n = 1000
+# fraction_protected = 0.5
+#
+# unprotected_cdf = fico_cdf["White"].values
+# protected_cdf = fico_cdf["Black"].values
+# shifted_scores = (fico_cdf.index.values * 10) + 10
+#
+# s = (
+#     np.random.rand(n, 1) < fraction_protected
+# ).astype(int).squeeze()
+#
+# s0_idx = np.where(s == 0)[0]
+# s1_idx = np.where(s == 1)[0]
+#
+# rands_s0 = np.random.randint(0, 10 ** PRECISION, len(s0_idx)) / float(10 ** PRECISION)
+# rands_s1 = np.random.randint(0, 10 ** PRECISION, len(s1_idx)) / float(10 ** PRECISION)
+#
+# previous_unprotected_threshold = -1
+# previous_protected_threshold = -1
+#
+# for score_idx, shifted_score in enumerate(shifted_scores):
+#     unprotected_threshold = unprotected_cdf[score_idx]
+#     protected_threshold = protected_cdf[score_idx]
+#
+#     rands_s0[(rands_s0 > previous_unprotected_threshold) & (rands_s0 <= unprotected_threshold)] = shifted_score
+#     rands_s1[(rands_s1 > previous_protected_threshold) & (rands_s1 <= protected_threshold)] = shifted_score
+#
+#     previous_unprotected_threshold = unprotected_threshold
+#     previous_protected_threshold = protected_threshold
+#
+# x = np.zeros(n)
+# x[s0_idx] = rands_s0
+# x[s1_idx] = rands_s1
+# x = (x - 10) / 1000
+#
+# #======================================================================#
+#
+# y = np.full(n, -1)
+# s0_idx = np.where(s == 0)[0]
+# s1_idx = np.where(s == 1)[0]
+#
+# scores =  fico_data["performance"]["Black"].index.values / 100.0
+# non_defaulters_protected =  fico_data["performance"]["Black"].values
+# non_defaulters_unprotected =  fico_data["performance"]["White"].values
+#
+# for idx, score in enumerate(scores):
+#     x_current_score_idx = np.where(x == score)[0]
+#
+#     s_current_score = s[x_current_score_idx]
+#     x_s0_current_score_idx = x_current_score_idx[s_current_score == 0]
+#     x_s1_current_score_idx = x_current_score_idx[s_current_score == 1]
+#     # x_s0_current_score_idx = np.where(x_s0 == score)[0]
+#     # x_s1_current_score_idx = np.where(x_s1 == score)[0]
+#
+#     y_unprotected = (
+#         np.random.rand(len(x_s0_current_score_idx), 1) < non_defaulters_unprotected[idx]
+#     ).astype(int).squeeze()
+#     y_protected = (
+#         np.random.rand(len(x_s1_current_score_idx), 1) < non_defaulters_protected[idx]
+#     ).astype(int).squeeze()
+#
+#     y[x_s0_current_score_idx] = y_unprotected
+#     y[x_s1_current_score_idx] = y_protected
+#
+# print(x)
 
+from src.distribution import FICODistribution
 
-def util_func(**util_params):
-    util = cost_utility(cost_factor=0.142, **util_params)
-    return util
+dist = FICODistribution(fraction_protected=0.2, bias=True)
 
-
-training_parameters = {
-    'save': True,
-    'experiment_name': 'test',
-    'save_path': './',
-    'model': {
-        'benefit_function': demographic_parity,
-        'utility_function': util_func,
-        'fairness_function': fairness_function,
-        'fairness_gradient_function': fairness_gradient_function,
-        'feature_map': IdentityFeatureMap(dim_theta),
-        'learn_on_entire_history': False,
-        'use_sensitve_attributes': False,
-        'bias': bias,
-        'initial_theta': [0.0, 0.0]
-    },
-    'parameter_optimization': {
-        'time_steps': 50,
-        'epochs': 1,
-        'batch_size': 256,
-        'learning_rate': 1,
-        'decay_rate': 1,
-        'decay_step': 10000,
-        'num_decisions': 128 * 256
-    },
-    'data': {
-        'distribution': UncalibratedScore(bias=bias, fraction_protected=0.5),
-        'num_test_samples': 8192
-    }
-}
-
-
-# x, s, y = load_dataset("./src/dat/compas/compas.npz")
-# x, x_test, y, y_test, s, s_test = train_test_split(x, y, s, test_size=0.8)
-# dist = ResamplingDistribution(load_dataset("./dat/compas/compas.npz"), 0.2, bias=bias)
-
-# training_parameters["model"]["initial_lambda"] = 0.000007799
-training_parameters["model"]["initial_lambda"] = 0
-
-# training_parameters["save_path"] = "/home/fkretschmar/Documents/master-thesis/res/test/uncalibrated/time"
-# lambdas = np.logspace(-1, 1, base=10, endpoint=True, num=3)
-# lambdas = np.insert(arr=lambdas, obj=0, values=[0.0])
-training_parameters["lagrangian_optimization"] = {
-    # 'duality_gap': 0.00001,
-    'iterations': 20,
-    'epochs': 1,
-    'batch_size': 256,
-    'learning_rate': 0.01,
-    'decay_rate': 1,
-    'decay_step': 10000,
-    'num_decisions': 128 * 256
-}
-
-statistics, model_parameters, run_path = train(training_parameters, iterations=3, asynchronous=False)
-# statistics, run_path = train(training_parameters, fairness_rates=lambdas, iterations=5, verbose=True, asynchronous=False)
-# statistics, run_path = train(training_parameters, fairness_rates=[0.0], iterations=5, verbose=True, asynchronous=False)
-
-#plot_median(statistics, model_parameters=model_parameters)
-plot_median(statistics, model_parameters=None)
-#plot_mean(statistics, "{}/results_mean_lambdas.png".format(run_path))
-
-#plot_median_over_lambdas(statistics, "{}/results_median_lambdas.png".format(run_path))
-
-#plot_median_over_time(statistics, "{}/results_median.png".format(run_path))
+train = dist.sample_train_dataset(10000)
+print(train)
