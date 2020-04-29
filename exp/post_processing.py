5767020e57792b87d941cbc45fb90ad8b9915a2b
Floyd Kretschmar <fkretschmar@googlemail.com>
2020-04-29 11:58:29 +0200

    save final mean, median, iqr and stddev of utility for each parameter setting in csv/xls

--- exp/post_processing.py
+++ exp/post_processing.py
@@ -1,8 +1,9 @@
 import argparse
-import json
 import os
 import sys
 from pathlib import Path
+
+import pandas as pd
 
 module_path = os.path.abspath(os.path.join('..'))
 
@@ -19,28 +20,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input_path', type=str, required=True, help="the path containing the raw data")
 parser.add_argument('-o', '--output_path', type=str, required=True,
                     help="the path into which the processed data will be saved")
+parser.add_argument('-a', '--analyze_only', action='store_true')
 args = parser.parse_args()
 
 cost_directories = [os.path.join(args.input_path, cost) for cost in os.listdir(args.input_path)
                     if os.path.isdir(os.path.join(args.input_path, cost))]
 
-best_parameter_settings = []
+results = []
 
 for cost in cost_directories:
-    learning_rates = [os.path.join(cost, lr) for lr in os.listdir(cost)
+    learning_rates = [(os.path.join(cost, lr), lr) for lr in os.listdir(cost)
                       if os.path.isdir(os.path.join(cost, lr))]
 
-    for learning_rate in learning_rates:
-        parameter_settings = [os.path.join(learning_rate, parameter_setting) for parameter_setting in
-                              os.listdir(learning_rate)
-                              if os.path.isdir(os.path.join(learning_rate, parameter_setting))]
+    for learning_rate_path, learning_rate in learning_rates:
+        parameter_settings = [(os.path.join(learning_rate_path, parameter_setting), parameter_setting) for
+                              parameter_setting in os.listdir(learning_rate_path) if
+                              os.path.isdir(os.path.join(learning_rate_path, parameter_setting))]
 
-        best_utility = float('-inf')
-        best_utility_path = ""
-
-        for parameter_setting in parameter_settings:
-            runs = [os.path.join(parameter_setting, run) for run in os.listdir(parameter_setting)
-                    if os.path.isdir(os.path.join(parameter_setting, run))]
+        for parameter_setting_path, parameter_setting in parameter_settings:
+            runs = [os.path.join(parameter_setting_path, run) for run in os.listdir(parameter_setting_path)
+                    if os.path.isdir(os.path.join(parameter_setting_path, run))]
 
             run_results = []
 
@@ -64,26 +63,31 @@ for cost in cost_directories:
 
             # merge statistics and model parameters
             statistcs_over_runs, model_parameters = _process_results(run_results)
-            output_path = parameter_setting.replace(args.input_path, args.output_path)
+            output_path = parameter_setting_path.replace(args.input_path, args.output_path)
 
             # mkdir output path if necessary
             Path(output_path).mkdir(parents=True, exist_ok=True)
 
             statistics = statistcs_over_runs[0]
 
-            # save the merged statistics, parameters and plot them
-            _save_results(output_path, statistics, model_parameters)
-            plot_mean(statistics, file_path=os.path.join(output_path, "results_mean.png"))
-            plot_median(statistics, file_path=os.path.join(output_path, "results_median.png"))
+            if not args.analyze_only:
+                # save the merged statistics, parameters and plot them
+                _save_results(output_path, statistics, model_parameters)
+                plot_mean(statistics, file_path=os.path.join(output_path, "results_mean.png"))
+                plot_median(statistics, file_path=os.path.join(output_path, "results_median.png"))
 
-            # update the best median utility parameter setting for the current learning rate
-            current_utility = statistics.performance(Statistics.UTILITY, Statistics.MEDIAN)[-1]
-            if current_utility > best_utility:
-                best_utility = current_utility
-                best_utility_path = output_path
+            result_row = [learning_rate]
+            result_row.append(parameter_setting)
+            result_row.append(statistics.performance(Statistics.UTILITY, Statistics.MEDIAN)[-1])
+            result_row.append(statistics.performance(Statistics.UTILITY, Statistics.THIRD_QUARTILE)[-1] -
+                              statistics.performance(Statistics.UTILITY, Statistics.FIRST_QUARTILE)[-1])
+            result_row.append(statistics.performance(Statistics.UTILITY, Statistics.MEAN)[-1])
+            result_row.append(statistics.performance(Statistics.UTILITY, Statistics.STANDARD_DEVIATION)[-1])
 
-        best_parameter_settings.append((best_utility_path, best_utility))
+            results.append(result_row)
+            print("finished processing {}".format(parameter_setting_path))
 
-res = os.path.join(args.output_path, "best_results.txt")
-with open(res, 'w') as file:
-    json.dump(best_parameter_settings, file)
+results_df = pd.DataFrame(results)
+results_df.columns = ['lr', 'parameters', 'median', 'iqr', 'mean', 'std_dev']
+results_df.to_csv(os.path.join(args.output_path, 'results.csv'))
+results_df.to_excel(os.path.join(args.output_path, 'results.xls'))
