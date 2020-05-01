f66ad9056b0038a2876b3a6fb249e698a5b90d7b
Floyd Kretschmar <fkretschmar@googlemail.com>
2020-05-01 16:40:29 +0200

    added post processing of fairness runs

--- exp/post_processing.py
+++ exp/post_processing.py
@@ -3,6 +3,7 @@ import os
 import sys
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 module_path = os.path.abspath(os.path.join('..'))
@@ -10,7 +11,7 @@ module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
 
-from src.training_evaluation import Statistics, _unserialize_dictionary
+from src.training_evaluation import Statistics, _unserialize_dictionary, MultiStatistics
 from src.util import load_dictionary
 from src.training import _save_results, _process_results
 from src.plotting import plot_mean, plot_median
@@ -22,7 +23,47 @@ parser.add_argument('-o', '--output_path', type=str, required=True,
                     help="the path into which the processed data will be saved")
 parser.add_argument('-s', '--save', action='store_true')
 parser.add_argument('-f', '--fairness', action='store_true')
+parser.add_argument('-a', '--analyze', action='store_true')
 args = parser.parse_args()
+
+
+def fairness(runs):
+    fairness_rates = np.array([float(run) for run_path, run in runs], dtype=float)
+    run_paths = [run_path for run_path, run in runs]
+    sorted_fairness_idx = np.argsort(fairness_rates)
+    overall_statistics = MultiStatistics.build("log", fairness_rates[sorted_fairness_idx], "Lambda")
+
+    # load statistics and model parameters for all runs of a parameter setting
+    for lambda_idx in sorted_fairness_idx:
+        statistics_path = os.path.join(run_paths[lambda_idx], "statistics.json")
+        serialized_statistics = load_dictionary(statistics_path)
+        statistics = Statistics.build_from_serialized_dictionary(serialized_statistics)
+        overall_statistics.log_run(statistics)
+
+    return overall_statistics
+
+
+def no_fairness(runs):
+    run_results = []
+
+    # load statistics and model parameters for all runs of a parameter setting
+    for run_path, run in runs:
+        statistics_path = os.path.join(run_path, "statistics.json")
+        serialized_statistics = load_dictionary(statistics_path)
+        statistics = Statistics.build_from_serialized_dictionary(serialized_statistics)
+
+        model_parameter_path = os.path.join(run_path, "models.json")
+        serialized_model_parameters = load_dictionary(model_parameter_path)
+        model_parameters = _unserialize_dictionary(serialized_model_parameters)
+        model_parameters = model_parameters['0']['0']
+
+        run_results.append([(statistics, model_parameters)])
+
+    # merge statistics and model parameters
+    statistcs_over_runs, model_parameters = _process_results(run_results)
+
+    return statistcs_over_runs[0], model_parameters
+
 
 cost_directories = [os.path.join(args.input_path, cost) for cost in os.listdir(args.input_path)
                     if os.path.isdir(os.path.join(args.input_path, cost))]
@@ -39,56 +80,53 @@ for cost in cost_directories:
                               os.path.isdir(os.path.join(learning_rate_path, parameter_setting))]
 
         for parameter_setting_path, parameter_setting in parameter_settings:
-            runs = [os.path.join(parameter_setting_path, run) for run in os.listdir(parameter_setting_path)
+            runs = [(os.path.join(parameter_setting_path, run), run) for run in os.listdir(parameter_setting_path)
                     if os.path.isdir(os.path.join(parameter_setting_path, run))]
 
-            run_results = []
+            if args.analyze:
+                result_row = [learning_rate]
+                result_row.append(parameter_setting)
+            else:
+                result_row = None
 
-            # load statistics and model parameters for all runs of a parameter setting
-            for run in runs:
-                statistics_path = os.path.join(run, "statistics.json")
-                serialized_statistics = load_dictionary(statistics_path)
-                statistics = Statistics.build_from_serialized_dictionary(serialized_statistics)
+            if args.fairness:
+                statistics = fairness(runs)
+                model_parameters = None
 
-                # if the x values = range then we are merging old buggy results > hardcode x range
-                # if statistics.results[Statistics.X_VALUES] == "range":
-                #     statistics.results[Statistics.X_VALUES] = list(
-                #         range(0, statistics.results["all"][Statistics.UTILITY].shape[0]))
+                if result_row is not None:
+                    result_row.append(statistics.performance(Statistics.UTILITY, Statistics.MEDIAN)[0])
+                    result_row.append(statistics.performance(Statistics.UTILITY, Statistics.MEDIAN)[-1])
+                    result_row.append((statistics.performance(Statistics.UTILITY, Statistics.THIRD_QUARTILE) -
+                                       statistics.performance(Statistics.UTILITY, Statistics.FIRST_QUARTILE)).mean(
+                        axis=0))
+                    result_columns = ['lr', 'parameters', 'first_median', 'final_median', 'avg_iqr']
+                    results.append(result_row)
+            else:
+                statistics, model_parameters = no_fairness(runs)
 
-                model_parameter_path = os.path.join(run, "models.json")
-                serialized_model_parameters = load_dictionary(model_parameter_path)
-                model_parameters = _unserialize_dictionary(serialized_model_parameters)
-                model_parameters = model_parameters['0']['0']
-
-                run_results.append([(statistics, model_parameters)])
-
-            # merge statistics and model parameters
-            statistcs_over_runs, model_parameters = _process_results(run_results)
-            output_path = parameter_setting_path.replace(args.input_path, args.output_path)
-
-            # mkdir output path if necessary
-            Path(output_path).mkdir(parents=True, exist_ok=True)
-
-            statistics = statistcs_over_runs[0]
+                if result_row is not None:
+                    result_row.append(statistics.performance(Statistics.UTILITY, Statistics.MEDIAN)[-1])
+                    result_row.append(statistics.performance(Statistics.UTILITY, Statistics.THIRD_QUARTILE)[-1] -
+                                      statistics.performance(Statistics.UTILITY, Statistics.FIRST_QUARTILE)[-1])
+                    result_row.append(statistics.performance(Statistics.UTILITY, Statistics.MEAN)[-1])
+                    result_row.append(statistics.performance(Statistics.UTILITY, Statistics.STANDARD_DEVIATION)[-1])
+                    result_columns = ['lr', 'parameters', 'final_median', 'final_iqr', 'final_mean', 'final_std_dev']
+                    results.append(result_row)
 
             if args.save:
+                output_path = parameter_setting_path.replace(args.input_path, args.output_path)
+                # mkdir output path if necessary
+                Path(output_path).mkdir(parents=True, exist_ok=True)
+
                 # save the merged statistics, parameters and plot them
                 _save_results(output_path, statistics, model_parameters)
                 plot_mean(statistics, file_path=os.path.join(output_path, "results_mean.png"))
                 plot_median(statistics, file_path=os.path.join(output_path, "results_median.png"))
 
-            result_row = [learning_rate]
-            result_row.append(parameter_setting)
-            result_row.append(statistics.performance(Statistics.UTILITY, Statistics.MEDIAN)[-1])
-            result_row.append(statistics.performance(Statistics.UTILITY, Statistics.THIRD_QUARTILE)[-1] -
-                              statistics.performance(Statistics.UTILITY, Statistics.FIRST_QUARTILE)[-1])
-            result_row.append(statistics.performance(Statistics.UTILITY, Statistics.MEAN)[-1])
-            result_row.append(statistics.performance(Statistics.UTILITY, Statistics.STANDARD_DEVIATION)[-1])
-
-            results.append(result_row)
             print("finished processing {}".format(parameter_setting_path))
 
-results_df = pd.DataFrame(results)
-results_df.columns = ['lr', 'parameters', 'median', 'iqr', 'mean', 'std_dev']
-results_df.to_csv(os.path.join(args.output_path, 'results.csv'))
-results_df.to_excel(os.path.join(args.output_path, 'results.xls'))
+if args.analyze:
+    results_df = pd.DataFrame(results)
+    results_df.columns = result_columns
+    results_df.to_csv(os.path.join(args.output_path, 'results.csv'))
+    results_df.to_excel(os.path.join(args.output_path, 'results.xls'))
