fe98b99d78844bd60ab652c2459325be1f0bc96f
Floyd Kretschmar <fkretschmar@googlemail.com>
2020-06-22 15:11:17 +0200

    refactored adam optimization to generalize even further

--- src/functions.py
+++ src/functions.py
@@ -5,7 +5,9 @@ root_path = os.path.abspath(os.path.join('.'))
 if root_path not in sys.path:
     sys.path.append(root_path)
 
+import numpy as np
 from src.util import mean
+
 
 ####################### UTILITY FUNCTIONS #######################
 
@@ -20,9 +22,11 @@ def _utility_core(cost_factor, **utility_parameters):
 
     return utility
 
+
 def cost_utility(cost_factor=0.5, **utility_parameters):
     utility = _utility_core(cost_factor, **utility_parameters)
     return mean(utility, axis=0)
+
 
 def cost_utility_gradient(cost_factor=0.5, **utility_parameters):
     utility = _utility_core(cost_factor=cost_factor, **utility_parameters)
@@ -31,9 +35,38 @@ def cost_utility_gradient(cost_factor=0.5, **utility_parameters):
     utility_grad = log_policy_gradient * utility
     return mean(utility_grad, axis=0)
 
+
 def cost_utility_probability(cost_factor=0.5, **utility_parameters):
     decision_probabilities = utility_parameters["decision_probabilities"]
     y = utility_parameters["y"]
 
     return mean(decision_probabilities * (y - cost_factor), axis=0)
 
+
+####################### OPTIMIZER FUNCTIONS #######################
+
+def initialize_adam():
+    return {
+        "beta_1": 0.9,
+        "beta_2": 0.999,
+        "v": 0,
+        "m": 0,
+        "t": 1,
+        "epsilon": 0.0000001
+    }
+
+def adam_update(update_parameters, gradient):
+    update_parameters["m"] = update_parameters["beta_1"] * update_parameters["m"] + (
+                1 - update_parameters["beta_1"]) * gradient
+    update_parameters["v"] = update_parameters["beta_2"] * update_parameters["v"] + (
+                1 - update_parameters["beta_2"]) * np.power(gradient, 2)
+    m_hat = update_parameters["m"] / (1 - np.power(update_parameters["beta_1"], update_parameters["t"]))
+    v_hat = update_parameters["v"] / (1 - np.power(update_parameters["beta_2"], update_parameters["t"]))
+    update_parameters["t"] += 1
+    return m_hat / (np.sqrt(v_hat) + update_parameters["epsilon"])
+
+def initialize_sgd():
+    return { }
+
+def sgd_update(update_parameters, gradient):
+    return gradient
--- src/optimization.py
+++ src/optimization.py
@@ -12,6 +12,7 @@ root_path = os.path.abspath(os.path.join('.'))
 if root_path not in sys.path:
     sys.path.append(root_path)
 
+from src.functions import adam_update, initialize_adam
 
 ########################################$ OPTIMIZERS ####################################################
 
@@ -20,31 +21,17 @@ if root_path not in sys.path:
 class StochasticGradientOptimizer:
     """ The base class for stochastic gradient methods of optimization. """
 
-    def __init__(self, policy, optimization_target):
+    def __init__(self,
+                 policy,
+                 optimization_target,
+                 fairness_update_function=adam_update,
+                 fairness_parameter_init_funciton=initialize_adam):
         super().__init__()
         self._optimization_target = optimization_target
         self._policy = policy
-        self.fairness_update_parameters = self._initialize_update_parameters()
-
-    def _initialize_update_parameters(self):
-        return {
-            "beta_1": 0.9,
-            "beta_2": 0.999,
-            "v": 0,
-            "m": 0,
-            "t": 1,
-            "epsilon": 0.0000001
-        }
-
-    def _update_parameters_and_calculate_gradient(self, update_parameters, gradient):
-        update_parameters["m"] = update_parameters["beta_1"] * update_parameters["m"] + (
-                    1 - update_parameters["beta_1"]) * gradient
-        update_parameters["v"] = update_parameters["beta_2"] * update_parameters["v"] + (
-                    1 - update_parameters["beta_2"]) * np.power(gradient, 2)
-        m_hat = update_parameters["m"] / (1 - np.power(update_parameters["beta_1"], update_parameters["t"]))
-        v_hat = update_parameters["v"] / (1 - np.power(update_parameters["beta_2"], update_parameters["t"]))
-        update_parameters["t"] += 1
-        return m_hat / (np.sqrt(v_hat) + update_parameters["epsilon"])
+        self._fairness_parameter_init_funciton = fairness_parameter_init_funciton
+        self._fairness_update_function = fairness_update_function
+        self._fairness_update_parameters = self._fairness_parameter_init_funciton()
 
     @property
     def optimization_target(self):
@@ -144,7 +131,7 @@ class StochasticGradientOptimizer:
                                                                             decision_probabilities=decision_probability_batch,
                                                                             ips_weights=ips_weights_batch)
 
-            adam_gradient = self._update_parameters_and_calculate_gradient(self.fairness_update_parameters, gradient)
+            adam_gradient = self._fairness_update_function(self._fairness_update_parameters, gradient)
             new_fairness_rate = self.optimization_target.fairness_rate + learning_rate * adam_gradient
             self.optimization_target.fairness_rate = new_fairness_rate
 
